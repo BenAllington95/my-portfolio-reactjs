26071705529f4b6cbddf1859c134708318fcbedf
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-01-15 12:50:07 +0000

    Deleted files, added new styles

---
+++ my-app/src/Header.js
@@ -0,0 +1,22 @@
+
+import hamburgerIcon from "./images/reorder-three-outline.svg";
+
+
+function Header() {
+    return (
+      <div className="header">
+        <div className="header-top">
+            <h2 className="header-title">ben.allington</h2>
+            <img className="hamburger-icon" src={hamburgerIcon} alt="hamburger-icon"/>
+        </div>
+        <ul className="nav-list">
+            <li className="nav-list-items">Home</li>
+            <li className="nav-list-items">About</li>
+            <li className="nav-list-items">Contact</li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Header;
+  
