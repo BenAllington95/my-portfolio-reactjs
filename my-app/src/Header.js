9ebe16b91841b5cb20421e73f54103114a698060
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-01-16 17:01:59 +0000

    Updated Header Position Absolute

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -6,14 +6,14 @@ function Header() {
     return (
       <div className="header">
         <div className="header-top">
-            <h2 className="header-title">ben.<span className="white-text">allington</span></h2>
+            <h2 className="header-title">Ben.<span className="white-text">allington</span></h2>
             <img className="hamburger-icon" src={hamburgerIcon} alt="hamburger-icon"/>
         </div>
         <ul className="nav-list">
             <li className="nav-list-items"><ion-icon name="home-sharp"></ion-icon></li>
             <li className="nav-list-items">About</li>
             <li className="nav-list-items">Contact</li>
-            <li className="nav-list-items"><ion-icon name="moon-sharp"></ion-icon></li>
+            <li className="nav-list-items"><ion-icon className="moon-sharp" name="moon-sharp"></ion-icon></li>
         </ul>
       </div>
     );
