32b2f0e861e3cb80f14e8636189a563d5be5e871
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-01-17 17:02:50 +0000

    New React File, Adjustments to Nav

---
+++ my-app/src/About.js
@@ -0,0 +1,11 @@
+
+function About() {
+    return (
+      <div className="about">
+        <h1>about</h1>
+      </div>
+    );
+  }
+
+  
+  export default About;
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 
 import Header from './Header';
 import Main from './Main';
+import About from './About';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <About />
     </div>
   );
 }
