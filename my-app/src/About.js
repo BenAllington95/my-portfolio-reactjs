42a252fc7b4679a75c8b6b11279d70c6233cdbcc
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-01-18 19:21:32 +0000

    Added About text

--- my-app/src/About.js
+++ my-app/src/About.js
@@ -2,10 +2,15 @@
 function About() {
     return (
       <div className="about">
-        <h1>about</h1>
+          <div className="container">
+            <h1 className="about-heading">about</h1>
+            <p className="about-text">Hi, i'm Ben, an aspiring Front-End Web Developer based in the UK.</p>
+            <p className="about-text">I began to code in 2021 and started with languages such as HTML, CSS and Javascript. Since learning to code, I have always enjoyed the process of problem solving and the feeling of achievement when you manage to solve the outcome you were working towards.</p>
+            <p className="about-text">Looking back prior to when I started to code, I believe having the experience of creating formulas within Excel and coding VBA Macros gave myself a better understanding of what is required to learn Javascript etc.</p>
+            <p className="about-text">Feel free to head over to the Projects section to see the sites I have made so far!</p>
+          </div>
       </div>
     );
   }
-
-  
+    
   export default About;
