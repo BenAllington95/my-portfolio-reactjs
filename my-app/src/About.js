314846accd055e405764d9e63804f2286926388a
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-01-18 23:09:22 +0000

    New profile img, added text to about section

--- my-app/src/About.js
+++ my-app/src/About.js
@@ -4,8 +4,9 @@ function About() {
       <div className="about" id="about">
           <div className="container">
             <h2 className="sub-heading">about</h2>
+            <img className="about-img" src={require("./images/profile-img.png")} />
             <p className="about-text">Hi, i'm Ben, an aspiring Front-End Web Developer based in the UK.</p>
-            <p className="about-text">I began to code in 2021 and started with languages such as HTML, CSS and Javascript. Since learning to code, I have always enjoyed the process of problem solving and the feeling of achievement when you manage to solve the outcome you were working towards.</p>
+            <p className="about-text">I began to code in 2021 and started with languages such as HTML, CSS, Javascript, React and Git/Github. Since learning to code, I have always enjoyed the process of problem solving and the feeling of achievement when you manage to solve the outcome you were working towards.</p>
             <p className="about-text">Looking back prior to when I started to code, I believe having the experience of creating formulas within Microsoft Excel and coding VBA Macros gave myself a better understanding of what is required to learn Javascript etc.</p>
             <p className="about-text">Feel free to head over to the <span className="yellow-text"><a href="#projects">projects</a></span> section to see the sites I have made so far!</p>
           </div>
