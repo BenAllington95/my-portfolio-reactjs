978506aee84041c6b868ca2ce6d5b8b406094763
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-01-18 19:32:43 +0000

    Adjust styles on about.js

--- my-app/src/About.js
+++ my-app/src/About.js
@@ -1,11 +1,10 @@
-
 function About() {
     return (
       <div className="about">
           <div className="container">
-            <h1 className="about-heading">about</h1>
-            <p className="about-text">Hi, i'm Ben, an aspiring Front-End Web Developer based in the UK.</p>
-            <p className="about-text">I began to code in 2021 and started with languages such as HTML, CSS and Javascript. Since learning to code, I have always enjoyed the process of problem solving and the feeling of achievement when you manage to solve the outcome you were working towards.</p>
+            <h2 className="about-heading">about</h2>
+            <p className="about-text">Hi, i'm Ben, an aspiring Front-End Developer based in the UK.</p>
+            <p className="about-text">I began to code in 2021 and have focused on improving my knowledge with languages such as HTML, CSS, Javascript and React. Since learning to code, I  always enjoy the process of problem solving and the feeling of achievement when you manage to solve the outcome you were working towards.</p>
             <p className="about-text">Looking back prior to when I started to code, I believe having the experience of creating formulas within Excel and coding VBA Macros gave myself a better understanding of what is required to learn Javascript etc.</p>
             <p className="about-text">Feel free to head over to the Projects section to see the sites I have made so far!</p>
           </div>
