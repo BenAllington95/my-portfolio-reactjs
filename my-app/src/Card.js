b04652520fccb9c601cfa4e5e6d2a88ac080eb69
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-01-19 17:28:08 +0000

    Added buttons for card props

--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -4,6 +4,10 @@ export default function Card(props) {
         <div className="card">
             <div className="card-box-text">
                 <h3 className="card-title">{props.title}</h3>
+                <div className="card-links">
+                    <span className="card-link">View Code</span>
+                    <span className="card-link">View Site</span>
+                </div>
             </div>
             <img className="card-img" src={props.url}/>
         </div>
