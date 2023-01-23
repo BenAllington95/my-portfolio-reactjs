2664a478b8272709cd9f7d6792233bcbbf662b10
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-01-23 19:46:04 +0000

    Amended card links

--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -4,11 +4,11 @@ export default function Card(props) {
             <div className="card-box-text">
                 <h3 className="card-title">{props.title}</h3>
                 <div className="card-links">
-                    <span className="card-link">View Code</span>
-                    <span className="card-link">View Site</span>
+                    <a href="https//www.google.com" className="card-link"><span>View Code</span></a>
+                    <a href="https//www.google.com" className="card-link"><span>View Site</span></a>
                 </div>
             </div>
-            <img className="card-img" src={props.url}/>
+            <img className="card-img" src={props.url} alt={props.title}/>
         </div>
     )
 }
