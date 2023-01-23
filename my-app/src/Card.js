951545f9683d4ffcd7e3cd2f92d78ff4b7cc4374
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-01-23 19:59:24 +0000

    Added data to project-data.js

--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -4,8 +4,8 @@ export default function Card(props) {
             <div className="card-box-text">
                 <h3 className="card-title">{props.title}</h3>
                 <div className="card-links">
-                    <a href="https//www.google.com" className="card-link"><span>View Code</span></a>
-                    <a href="https//www.google.com" className="card-link"><span>View Site</span></a>
+                    <a href="www.google.com" className="card-link" target="_blank" rel="noreferrer">View Code</a>
+                    <a href={props.siteUrl} className="card-link" target="_blank" rel="noreferrer">View Site</a>
                 </div>
             </div>
             <img className="card-img" src={props.url} alt={props.title}/>
--- my-app/src/project-data.js
+++ my-app/src/project-data.js
@@ -1,18 +1,22 @@
 export default [
     {
         title: 'Diner App',
-        url: require('./images/diner-img.png')
+        url: require('./images/diner-img.png'),
+        siteUrl: "https://bensdinerapp.netlify.app/"
     },
     {
         title: 'Password Generator',
-        url: require('./images/password-img.png')
+        url: require('./images/password-img.png'),
+        siteUrl: "https://getpasswordbyben.netlify.app/"
     },
     {
-        title: 'RGB Random Generator',
-        url: require('./images/rgb-generator-img.png')
+        title: 'Random RGB Generator',
+        url: require('./images/rgb-generator-img.png'),
+        siteUrl: "https://benallington-rba-generator.netlify.app/"
     },
     {
         title: 'Unit Converter',
-        url: require('./images/unit-converter-img.png')
+        url: require('./images/unit-converter-img.png'),
+        siteUrl: "https://benunitconverter.netlify.app/"
     }
 ]
