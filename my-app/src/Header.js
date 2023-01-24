ed9163ff6f2156f60aedc7d1e5215002b84dac2b
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-01-24 18:44:59 +0000

    Added fixed navbar

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -4,16 +4,16 @@ import hamburgerIcon from "./images/reorder-three-outline.svg";
 
 function Header() {
     return (
-      <div className="header">
+      <div id="header" className="header">
         <div className="header-top">
             <h2 className="header-title">ben.<span className="white-text">allington</span></h2>
             <img className="hamburger-icon" src={hamburgerIcon} alt="hamburger-icon"/>
         </div>
         <ul className="nav-list">
-            <li className="nav-list-items"><ion-icon name="home-sharp"></ion-icon></li>
-            <li className="nav-list-items"><a href="#about">About</a></li>
-            <li className="nav-list-items"><a href="#projects">Projects</a></li>
-            <li className="nav-list-items">Contact</li>
+            <a href="#header"><li className="nav-list-items"><ion-icon name="home-sharp"></ion-icon></li></a>
+            <a href="#about"><li className="nav-list-items">About</li></a>
+            <a href="#projects"><li className="nav-list-items">Projects</li></a>
+            <a href="#projects"><li className="nav-list-items">Contact</li></a>
             <li className="nav-list-items"><ion-icon className="moon-sharp" name="moon-sharp"></ion-icon></li>
         </ul>
       </div>
