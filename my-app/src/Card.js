2e8f61ef336e155ec86a13e0026bf73a674f97c7
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-01-29 14:11:56 +0000

    Added footer icons and styling, adjust card stylilng

--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -2,6 +2,7 @@ export default function Card(props) {
     return (
         
         <a href={props.siteUrl} className="card" target="_blank" rel="noreferrer">
+            <img className="card-img" src={props.url} alt={props.title}/>
             <div className="card-box-text">
                 <h3 className="card-title">{props.title}</h3>
                 <div className="card-links">
@@ -9,7 +10,6 @@ export default function Card(props) {
                     <a href={props.siteUrl} className="card-link" target="_blank" rel="noreferrer">View Site</a>
                 </div>
             </div>
-            <img className="card-img" src={props.url} alt={props.title}/>
         </a>
     )
 }
--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -1,9 +1,16 @@
-import linkedInLogo from "./images/linkedin-icon-18-256.png";
+
+
 
 function Footer() {
     return (
       <div className="footer" id="footer">
-        <img src={linkedInLogo} className="footer-icon" alt="linkedin-logo" />
+        <ul className="footer-list">
+            <a href="#header"><li className="footer-list-items"><ion-icon name="mail-sharp"></ion-icon></li></a>
+            <a href="#header"><li className="footer-list-items"><ion-icon name="logo-linkedin"></ion-icon></li></a>
+            <a href="#header"><li className="footer-list-items"><ion-icon name="logo-github"></ion-icon></li></a>
+            <a href="#header"><li className="footer-list-items"><ion-icon name="logo-discord"></ion-icon></li></a>
+        </ul>
+        <p className="footer-text">Designed by Ben Allington</p>
       </div>
     );
   }
