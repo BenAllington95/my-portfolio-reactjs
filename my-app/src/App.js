107b2ec98718770fa784664879fdf47c08280b34
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-01-30 20:03:38 +0000

    Added scroll lock when hamburger is active

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,25 +15,31 @@ function App() {
     backgroundColor: navDisplay ? "#FFD56F" : "#DCDCDC"
   }
 
+  function scrollLock() {
+    return navDisplay ? document.body.style.overflow = "auto" : document.body.style.overflow = "hidden"
+  }
+
   function toggleNavDisplay() {
     setNavDisplay(prevNav => !prevNav)
+    scrollLock()
   }
 
   return (
     <div className="App">
       <div className="header-top">
             <h2 className="header-title">ben.<span className="white-text">allington</span></h2>
-            <img className="hamburger-icon" style={styles} src={navDisplay ? closeIcon : hamburgerIcon} onClick={toggleNavDisplay} alt="hamburger-icon"/>
+            <img className="hamburger-icon" style={styles} src={hamburgerIcon} onClick={toggleNavDisplay} alt="hamburger-icon"/>
         </div>
         <div className={`hamburger-nav ${navDisplay ? "active-nav" : ""}`}>
-        <img className="hamburger-icon" style={styles} src={navDisplay ? closeIcon : hamburgerIcon} onClick={toggleNavDisplay} alt="hamburger-icon"/>
-        <ul className="nav-list">
+        <img className="hamburger-close-icon" style={styles} src={closeIcon} onClick={toggleNavDisplay} alt="hamburger-icon"/>
+        <ul className="hamburger-list">
           <a href="#header"><li className="hamburger-list-items"><ion-icon name="home-sharp"></ion-icon></li></a>
           <a href="#about"><li className="hamburger-list-items">About</li></a>
           <a href="#projects"><li className="hamburger-list-items">Projects</li></a>
           <a href="#footer"><li className="hamburger-list-items">Contact</li></a>
         </ul>
       </div>
+
       <Header />
       <Main />
       <About />
