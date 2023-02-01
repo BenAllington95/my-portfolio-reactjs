bf37bbb1b989189ecf81ae81af37a9f71accd3d7
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-02-01 19:39:05 +0000

    Added links to footer icons to direct to sites - new window

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -5,10 +5,10 @@ function Footer() {
     return (
       <div className="footer" id="footer">
         <ul className="footer-list">
-            <a href="#header"><li className="footer-list-items"><ion-icon name="mail-sharp"></ion-icon></li></a>
-            <a href="#header"><li className="footer-list-items"><ion-icon name="logo-linkedin"></ion-icon></li></a>
-            <a href="#header"><li className="footer-list-items"><ion-icon name="logo-github"></ion-icon></li></a>
-            <a href="#header"><li className="footer-list-items"><ion-icon name="logo-discord"></ion-icon></li></a>
+        <li className="footer-list-items"><a href="mailto: benallington1995@gmail.com"><ion-icon name="mail-sharp"></ion-icon></a></li>
+        <li className="footer-list-items"><a href="https://www.google.com" target="_blank" rel="noreferrer"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+        <li className="footer-list-items"><a href="https://www.github.com/BenAllington95" target="_blank" rel="noreferrer"><ion-icon name="logo-github"></ion-icon></a></li>
+        <li className="footer-list-items"><a href="https://discordapp.com/users/Ben#0462" target="_blank" rel="noreferrer"><ion-icon name="logo-discord"></ion-icon></a></li>
         </ul>
         <p className="footer-text">Designed by Ben Allington</p>
       </div>
