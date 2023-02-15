0b934ed5940c02b3165e07b97613d0ac44ce006e
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-02-15 17:39:29 +0000

    Changed card design - more width

--- my-app/src/Skills.js
+++ my-app/src/Skills.js
@@ -5,11 +5,26 @@ function Skills() {
         <div className="container reveal">
             <h2 className="sub-heading">Skills</h2>
             <div className="skill-items">
+              <div className="skill">
+                <h3 className="skill-title">HTML</h3>
                 <ion-icon name="logo-html5"></ion-icon>
+              </div> 
+              <div className="skill">
+                <h3 className="skill-title">CSS</h3>
                 <ion-icon name="logo-css3"></ion-icon>
+              </div> 
+              <div className="skill">
+                <h3 className="skill-title">Javascript</h3>
                 <ion-icon name="logo-javascript"></ion-icon>
+              </div> 
+              <div className="skill">
+                <h3 className="skill-title">React JS</h3>
                 <ion-icon name="logo-react"></ion-icon>
+              </div> 
+              <div className="skill">
+                <h3 className="skill-title">Github</h3>
                 <ion-icon name="git-branch-outline"></ion-icon>
+              </div> 
             </div>
         </div>
       </div>
