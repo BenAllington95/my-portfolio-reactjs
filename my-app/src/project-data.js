05a6d76bcbbafeedb73cde50ee767bb071611198
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-02-16 18:08:36 +0000

    Added more project templates to array

--- my-app/src/project-data.js
+++ my-app/src/project-data.js
@@ -4,36 +4,69 @@ export default [
         url: require('./images/diner-img.png'),
         description: "A site designed to order food for mobile users",
         siteUrl: "https://bensdinerapp.netlify.app/",
-        codeUrl: "https://github.com/BenAllington95/portfolio-website"
+        codeUrl: "https://github.com/BenAllington95/diner-app-vanillajs"
     },
     {
         title: 'Password Generator',
         url: require('./images/password-img.png'),
         description: "A site to generate a random password with a choice of symbols, numbers, length etc.",
-        siteUrl: "https://getpasswordbyben.netlify.app/"
+        siteUrl: "https://getpasswordbyben.netlify.app/",
+        codeUrl: "https://github.com/BenAllington95/password-generator-vanillajs"
     },
     {
         title: 'Random RGB Generator',
         url: require('./images/rgb-generator-img.png'),
         description: "Generate a random RGB colour",
-        siteUrl: "https://benallington-rba-generator.netlify.app/"
+        siteUrl: "https://benallington-rba-generator.netlify.app/",
+        codeUrl: "https://github.com/BenAllington95/instagram-clone-vanillajs"
     },
     {
         title: 'Unit Converter',
         url: require('./images/unit-converter-img.png'),
         description: "Input a number value to return a converted unit value",
-        siteUrl: "https://benunitconverter.netlify.app/"
+        siteUrl: "https://benunitconverter.netlify.app/",
+        codeUrl: "https://github.com/BenAllington95/unit-converter-vanillajs"
     },
     {
         title: 'User Review Section',
         url: require('./images/comments-img.png'),
         description: "A site for users to write a comment/review with their name",
-        siteUrl: "https://benallington-commentsection.netlify.app"
+        siteUrl: "https://benallington-commentsection.netlify.app",
+        codeUrl: "https://github.com/BenAllington95/comment-section-js"
     },
     {
-        title: 'Portfolio Website',
+        title: 'My Portfolio',
         url: require('./images/website-card-img.png'),
         description: "A site created with React to showcase my projects, skills etc.",
-        siteUrl: "#"
+        siteUrl: "#",
+        codeUrl: "https://github.com/BenAllington95/portfolio-website"
+    },
+    {
+        title: 'Instagram Clone',
+        url: require('./images/website-card-img.png'),
+        description: "A mobile styled app designed to emulate the Instagram app.",
+        siteUrl: "#",
+        codeUrl: "https://github.com/BenAllington95/instagram-clone-vanillajs"
+    },
+    {
+        title: 'Tinder Clone (Tindog)',
+        url: require('./images/website-card-img.png'),
+        description: "A mobile styled app to mimic the Tinder app, but with dogs instead.",
+        siteUrl: "#",
+        codeUrl: "https://github.com/BenAllington95/tinder-clone-scrimba-challenge-js"
+    },
+    {
+        title: 'Travel Journal',
+        url: require('./images/website-card-img.png'),
+        description: "A basic React app that showcases a travel journal.",
+        siteUrl: "#",
+        codeUrl: "https://github.com/BenAllington95/my-travel-journel-react-js"
+    },
+    {
+        title: 'Meme Generator',
+        url: require('./images/website-card-img.png'),
+        description: "A react app that contacts an API to generate memes with user input ",
+        siteUrl: "#",
+        codeUrl: "https://github.com/BenAllington95/my-travel-journel-react-js"
     },
 ]
