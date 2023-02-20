5e464862ce449e14c3f6329e634ea4be74226a9c
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-02-20 18:46:58 +0000

    Moved header back into header app from app.js using helper functions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import Main from './Main';
 import About from './About';
+import Header from './Header';
 import Skills from './Skills'
 import Projects from './Projects';
 import Footer from './Footer';
@@ -29,37 +30,11 @@ function App() {
 
   return (
     <div className="App" id={theme ? "" : "app-light"}>
-      <div className="header-top">
-            {/* <h2 className="header-title">Ben Allington</h2> */}
-            <div className="hamburger-icon-container">
-              <span className="hamburger-icon"><ion-icon onClick={toggleNavDisplay} name="reorder-three-outline"></ion-icon></span>
-            </div>
-            <ul className="nav-list">
-            <a href="#header"><li className="nav-list-items"><ion-icon name="home-sharp"></ion-icon></li></a>
-            <a href="#about"><li className="nav-list-items">About</li></a>
-            <a href="#skills"><li className="nav-list-items">Skills</li></a>
-            <a href="#projects"><li className="nav-list-items">Projects</li></a>
-            <a href="#footer"><li className="nav-list-items">Contact</li></a>
-            <li onClick={toggleTheme} className="nav-list-items"><ion-icon name={theme ? "moon-sharp" : "sunny-sharp"}></ion-icon></li>
-        </ul>
-        </div>
-        <div className={`hamburger-nav ${navDisplay ? "active-nav" : ""}`}>
-        <div className="hamburger-icon-container">
-              <span className="hamburger-close-icon"><ion-icon onClick={toggleNavDisplay} name="close-sharp"></ion-icon></span>
-            </div>
-        <ul className="hamburger-list">
-          <a href="#about"><div onClick={toggleNavDisplay}><li className="hamburger-list-items">About</li></div></a>
-          <a href="#skills"><div onClick={toggleNavDisplay}><li className="hamburger-list-items">Skills</li></div></a>
-          <a href="#projects"><div onClick={toggleNavDisplay}><li className="hamburger-list-items">Projects</li></div></a>
-          <a href="#footer"><div onClick={toggleNavDisplay}><li className="hamburger-list-items">Contact</li></div></a>
-          <li onClick={toggleTheme} className="nav-list-items"><ion-icon name={theme ? "moon-sharp" : "sunny-sharp"}></ion-icon></li>
-          <a href="mailto: benallington1995@gmail.com"><li className="hamburger-list-items"><ion-icon name="mail-sharp"></ion-icon></li></a>
-          <a href="https://www.linkedin.com/in/ben-allington" target="_blank" rel="noreferrer"><li className="hamburger-list-items"><ion-icon name="logo-linkedin"></ion-icon></li></a>
-          <a href="https://www.github.com/BenAllington95" target="_blank" rel="noreferrer"><li className="hamburger-list-items"><ion-icon name="logo-github"></ion-icon></li></a>
-          <a href="https://discordapp.com/users/Ben#0462" target="_blank" rel="noreferrer"><li className="hamburger-list-items"><ion-icon name="logo-discord"></ion-icon></li></a>
-        </ul>
-      </div>
-
+      <Header 
+      toggleTheme={toggleTheme}
+      toggleNavDisplay={toggleNavDisplay} 
+      theme={theme} 
+      navDisplay={navDisplay} />
       <Main />
       <About />
       <Skills />
--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -6,39 +6,41 @@ import React, { useState } from 'react';
 
 
 function Header(props) {
-
-    const [navDisplay, setNavDisplay] = useState(true)
-    const [theme, setTheme] = useState(true)
-
-
-    // const styles = {
-    //   backgroundColor: navDisplay ? "#FFD56F" : "#DCDCDC"
-    // }
-    
-    function toggleNavDisplay() {
-      setNavDisplay(prevDisplay => !prevDisplay)      
-    }
-
-    function toggleThemeMode() {
-      setTheme(prevTheme => !prevTheme)
-    }
-
-
   
 
     return (
-      <div id="header" className={`header ${theme ? 'dark' : 'light'}`}>
-        {/* <div className="header-top">
-            <h2 className="header-title">ben.<span className="white-text">allington</span></h2>
-            <img className="hamburger-icon" style={styles} src={navDisplay ? closeIcon : hamburgerIcon} onClick={toggleNavDisplay} alt="hamburger-icon"/>
-        </div> */}
-        <ul className="nav-list">
+      <div id="header">
+        <div className="header-top">
+            <div className="hamburger-icon-container">
+              <span className="hamburger-icon"><ion-icon onClick={props.toggleNavDisplay} name="reorder-three-outline"></ion-icon></span>
+            </div>
+            <ul className="nav-list">
             <a href="#header"><li className="nav-list-items"><ion-icon name="home-sharp"></ion-icon></li></a>
             <a href="#about"><li className="nav-list-items">About</li></a>
+            <a href="#skills"><li className="nav-list-items">Skills</li></a>
             <a href="#projects"><li className="nav-list-items">Projects</li></a>
             <a href="#footer"><li className="nav-list-items">Contact</li></a>
+            <li onClick={props.toggleTheme} className="nav-list-items"><ion-icon name={props.theme ? "moon-sharp" : "sunny-sharp"}></ion-icon></li>
+        </ul>
+        </div>
+        <div className={`hamburger-nav ${props.navDisplay ? "active-nav" : ""}`}>
+        <div className="hamburger-icon-container">
+              <span className="hamburger-close-icon"><ion-icon onClick={props.toggleNavDisplay} name="close-sharp"></ion-icon></span>
+            </div>
+        <ul className="hamburger-list">
+          <a href="#about"><div onClick={props.toggleNavDisplay}><li className="hamburger-list-items">About</li></div></a>
+          <a href="#skills"><div onClick={props.toggleNavDisplay}><li className="hamburger-list-items">Skills</li></div></a>
+          <a href="#projects"><div onClick={props.toggleNavDisplay}><li className="hamburger-list-items">Projects</li></div></a>
+          <a href="#footer"><div onClick={props.toggleNavDisplay}><li className="hamburger-list-items">Contact</li></div></a>
+          <li onClick={props.toggleTheme} className="nav-list-items"><ion-icon name={props.theme ? "moon-sharp" : "sunny-sharp"}></ion-icon></li>
+          <a href="mailto: benallington1995@gmail.com"><li className="hamburger-list-items"><ion-icon name="mail-sharp"></ion-icon></li></a>
+          <a href="https://www.linkedin.com/in/ben-allington" target="_blank" rel="noreferrer"><li className="hamburger-list-items"><ion-icon name="logo-linkedin"></ion-icon></li></a>
+          <a href="https://www.github.com/BenAllington95" target="_blank" rel="noreferrer"><li className="hamburger-list-items"><ion-icon name="logo-github"></ion-icon></li></a>
+          <a href="https://discordapp.com/users/Ben#0462" target="_blank" rel="noreferrer"><li className="hamburger-list-items"><ion-icon name="logo-discord"></ion-icon></li></a>
         </ul>
       </div>
+      </div>
+  
     );
   }
 
