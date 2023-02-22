a655b312736cfaacbea6cb23e0f097874c0c4262
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-02-22 00:04:35 +0000

    Remove sticky nav - causes error to page offset

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -40,16 +40,16 @@ function Header(props) {
     );
   }
 
-const navbar = document.querySelector('#header');
-let top = navbar.offsetTop;
-function stickynavbar() {
-  if (window.scrollY >= top) {    
-    navbar.classList.add('sticky');
-  } else {
-    navbar.classList.remove('sticky');    
-  }
-}
-window.addEventListener('scroll', stickynavbar);
+// const navbar = document.querySelector('#header');
+// let top = navbar.offsetTop;
+// function stickynavbar() {
+//   if (window.scrollY >= top) {    
+//     navbar.classList.add('sticky');
+//   } else {
+//     navbar.classList.remove('sticky');    
+//   }
+// }
+// window.addEventListener('scroll', stickynavbar);
 
   
   export default Header;
