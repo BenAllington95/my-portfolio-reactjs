48fba51b4e0549c92fd2c3602145981770724912
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-02-22 18:50:49 +0000

    Change accent-text to sub-colour

--- my-app/src/About.js
+++ my-app/src/About.js
@@ -8,11 +8,11 @@ function About() {
             <p className="about-text">Hi, I'm Ben, a self-taught Front End Developer from the UK.</p>
             <p className="about-text">I seek a role within a commercial organisation to continue my development and a career path suited to my skills and interests. I am a keen learner and always look for ways to improve my skills.</p>
             <p className="about-text">I began to code in 2021 through the Scrimba Front End Developer Course which guided me to create various websites using HTML, CSS, Javascript, and React.</p>
-            <p className="about-text">Feel free to head to the <span className="accent-text"><a href="#projects">projects</a></span> section to see the sites I have made so far!</p>
-            <h3 className="accent-text about-sub-heading">Hobbies</h3>
+            <p className="about-text">Feel free to head to the <span className="sub-colour"><a href="#projects">projects</a></span> section to see the sites I have made so far!</p>
+            <h3 className="sub-colour about-sub-heading">Hobbies</h3>
             <p className="about-text">I’m an active person who enjoys playing and watching sports in my free time, my favourites being football, basketball and F1.</p>
             <p className="about-text">I believe that my sporting background has always pushed me to improve myself in other areas.</p>
-            <p className="about-text">If you would like to get in touch with me, the best way is by sending an email to <a href="mailto: benallington1995@gmail.com"><span className="accent-text">benallington1995@gmail.com</span></a>.</p>
+            <p className="about-text">If you would like to get in touch with me, the best way is by sending an email to <a href="mailto: benallington1995@gmail.com"><span className="sub-colour">benallington1995@gmail.com</span></a>.</p>
           </div>
       </div>
     );
