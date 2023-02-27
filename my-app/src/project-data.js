0fe8b10763e6e0b4edc5d5f3140c9faa675d390f
BenAllington95 <103380285+BenAllington95@users.noreply.github.com>
2023-02-27 17:55:52 +0000

    Adjusted string quotes

--- my-app/src/project-data.js
+++ my-app/src/project-data.js
@@ -1,63 +1,63 @@
 export default [
     {
-        title: 'Diner App',
-        url: require('./images/diner-img.png'),
+        title: "Diner App",
+        url: require("./images/diner-img.png"),
         description: "Designed to order food for mobile users",
         siteUrl: "https://bensdinerapp.netlify.app/",
         codeUrl: "https://github.com/BenAllington95/diner-app-vanillajs"
     },
     {
-        title: 'To Do List',
-        url: require('./images/to-do-list-img.png'),
+        title: "To Do List",
+        url: require("./images/to-do-list-img.png"),
         description: "Add tasks to complete in your day, mark as complete or remove.",
         siteUrl: "https://benallington-todolist.netlify.app/",
         codeUrl: "https://github.com/BenAllington95/to-do-list-vanillajs"
     },
     {
-        title: 'Password Generator',
-        url: require('./images/password-img.png'),
+        title: "Password Generator",
+        url: require("./images/password-img.png"),
         description: "Generate a random password with a choice of symbols, numbers, length etc.",
         siteUrl: "https://benallington-passwordgenerator.netlify.app/",
         codeUrl: "https://github.com/BenAllington95/password-generator-vanillajs"
     },
     {
-        title: 'Travel Journal',
-        url: require('./images/travel-journal-img.png'),
+        title: "Travel Journal",
+        url: require("./images/travel-journal-img.png"),
         description: "A React App that showcases a travel journal.",
         siteUrl: "https://benallington-mylearningjournal-react.netlify.app/",
         codeUrl: "https://github.com/BenAllington95/my-travel-journel-react-js"
     },
     {
-        title: 'User Review Section',
-        url: require('./images/comments-img.png'),
+        title: "User Review Section",
+        url: require("./images/comments-img.png"),
         description: "A section for users to write a comment/review.",
         siteUrl: "https://benallington-commentsection.netlify.app",
         codeUrl: "https://github.com/BenAllington95/comment-section-js"
     },
     {
-        title: 'RGBA Generator',
-        url: require('./images/rgb-generator-img.png'),
+        title: "RGBA Generator",
+        url: require("./images/rgb-generator-img.png"),
         description: "Generate a random RGB colour.",
         siteUrl: "https://benallington-rgbacolorgenerator.netlify.app/",
         codeUrl: "https://github.com/BenAllington95/rgba-color-generator-js"
     },
     {
-        title: 'Unit Converter',
-        url: require('./images/unit-converter-img.png'),
+        title: "Unit Converter",
+        url: require("./images/unit-converter-img.png"),
         description: "Input a number to return a converted unit value.",
         siteUrl: "https://benunitconverter.netlify.app/",
         codeUrl: "https://github.com/BenAllington95/unit-converter-vanillajs"
     },
     {
-        title: 'My Portfolio',
-        url: require('./images/website-card-img.png'),
+        title: "My Portfolio",
+        url: require("./images/website-card-img.png"),
         description: "A React App to showcase my projects, skills etc.",
         siteUrl: "#",
         codeUrl: "https://github.com/BenAllington95/portfolio-website"
     },
     {
-        title: 'Tinder Clone (Tindog)',
-        url: require('./images/tinder-img.png'),
+        title: "Tinder Clone (Tindog)",
+        url: require("./images/tinder-img.png"),
         description: "A mobile styled app to mimic the Tinder app, but with dogs instead.",
         siteUrl: "https://benallington-tindog.netlify.app/",
         codeUrl: "https://github.com/BenAllington95/tinder-clone-scrimba-challenge-js"
@@ -65,8 +65,8 @@ export default [
 ]
 
 // {
-//     title: 'Meme Generator',
-//     url: require('./images/meme-generator-img.png'),
+//     title: "Meme Generator",
+//     url: require("./images/meme-generator-img.png"),
 //     description: "A React App that fetches an API to generate memes with user input ",
 //     siteUrl: "#",
 //     codeUrl: "https://github.com/BenAllington95/my-travel-journel-react-js"
